0866a636dacb1d428031a511fbb871ea408e0a5f
Opeoluwa7 <hazeofficial298@gmail.com>
2025-05-03 18:39:01 +0100

    test

--- script.sh
+++ script.sh
@@ -1,23 +1,29 @@
 #!/bin/zsh
 
-rm -rf dist
 
 echo "deleting dist"
 
+rm -rf dist
+
 sleep 3
 
+echo "Creating build"
 npm run build
 
-echo "creating build"
-
 sleep 3
+
+echo "git add"
 
 git add .
 
 sleep 3
 
+echo "git commit test"
+
 git commit -m "test"
 
 sleep 3
+
+echo "git push"
 
 git push origin main
