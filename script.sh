6391049a45c7b0bcbd84a9fe46e7fc664285b08a
Opeoluwa7 <hazeofficial298@gmail.com>
2025-05-03 18:39:54 +0100

    test

--- script.sh
+++ script.sh
@@ -5,24 +5,24 @@ echo "deleting dist"
 
 rm -rf dist
 
-sleep 3
+sleep 1
 
 echo "Creating build"
 npm run build
 
-sleep 3
+sleep 1
 
 echo "git add"
 
 git add .
 
-sleep 3
+sleep 1
 
 echo "git commit test"
 
 git commit -m "test"
 
-sleep 3
+sleep 1
 
 echo "git push"
 
