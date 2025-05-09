20dd9055128ac4ea52abce599f618e3c58287ee8
Opeoluwa7 <hazeofficial298@gmail.com>
2025-05-09 13:36:28 +0100

    test

--- dist/global/variables.js
+++ dist/global/variables.js
@@ -1,1 +1,8 @@
 "use strict";
+let title;
+let description;
+let status;
+let priority;
+let deadline;
+let user_id;
+var taskVar = [title, description, status, priority, deadline, user_id];
---
+++ src/global/variables.ts
@@ -0,0 +1,9 @@
+let title: string;
+let description: string;
+let status: string;
+let priority: string;
+let deadline: Date;
+let user_id: number
+
+
+var taskVar: any[] = [title, description, status, priority, deadline, user_id]
